2767326c95b405d068050c53f3ddfcdcf718365f
SmallE0529 <m0956571899@hotmail.com>
2017-07-13 16:17:12 +0800

    ADC serialport plot

---
+++ cc3200ADC/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace cc3200ADC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(66, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(66, 75);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(514, 318);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 437);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ cc3200ADC/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+
+namespace cc3200ADC
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort ports;
+        private const int SampleRate = 8;
+        private Int16[] InputDataArray = new Int16[SampleRate];
+        private static int cnt = 0;
+        private Thread cpuThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetAvailablePorts();
+            label1.Text = "COM";
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].Color = Color.Red;
+            chart1.Series[0].IsValueShownAsLabel = false;
+
+        }
+
+        void GetAvailablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        private void RescaleChart()
+        {
+            const int N = 200;
+
+            var points = chart1.Series[0].Points;
+            if (points.Count > N)
+            {
+                int xmax = points.Count - 1;
+                int xmin = points.Count - N;
+                chart1.ChartAreas[0].Axes[0].Minimum = xmin;
+                chart1.ChartAreas[0].Axes[0].Maximum = xmax;
+                double ymin = points.FindMinByValue("Y", xmin).YValues[0];
+                double ymax = points.FindMaxByValue("Y", xmin).YValues[0];
+                chart1.ChartAreas[0].Axes[1].Minimum = ymin;
+                chart1.ChartAreas[0].Axes[1].Maximum = ymax;
+            }
+
+        }
+
+        private void UpdateCpuChart(Int16[] data)
+        {
+            foreach (Int16 item in data)
+            {
+                chart1.Series[0].Points.AddY(item);
+
+            }
+            RescaleChart();
+        }
+
+        private void getPerformanceCounters()
+        {
+            while (true)
+            {
+                if (chart1.IsHandleCreated)
+                {
+                    //InputData = ports.ReadLine();
+                    InputDataArray[cnt] = Int16.Parse(ports.ReadLine());
+                    cnt++;
+                    if (cnt >= SampleRate)
+                    {
+                        cnt = 0;
+                        Int16[] InputDataArray2 = (Int16[])InputDataArray.Clone();
+                        //this.Invoke(new Action(() => UpdateCpuChart(InputDataArray2)));
+                        this.Invoke((MethodInvoker)delegate { UpdateCpuChart(InputDataArray2); });
+                    }
+                }
+                else
+                {
+                    //......
+                }
+                //Thread.Sleep(16);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Please Select Serial Port");
+            }
+            else
+            {
+                ports = new SerialPort(comboBox1.Text, 
+                                       115200, 
+                                       Parity.None, 
+                                       8, 
+                                       StopBits.One);
+                try
+                {
+                    ports.Open();
+                    label1.Text = "";
+                    cpuThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+                    cpuThread.IsBackground = true;
+                    cpuThread.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ports.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
